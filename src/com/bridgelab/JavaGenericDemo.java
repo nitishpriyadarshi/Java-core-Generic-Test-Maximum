feb80e8f8b9898f28c95b3d8be97eaba4430d587
nitis <nitish8152@gmail.com>
2022-10-11 13:33:39 +0530

    Added: Define Generic Class for Comparing 3 Object

--- src/com/bridgelab/JavaGenericDemo.java
+++ src/com/bridgelab/JavaGenericDemo.java
@@ -1,8 +1,16 @@
 package com.bridgelab;
 
-public class JavaGenericDemo {
+public class JavaGenericDemo   <T extends Comparable<T>>{
+    T a,b,c;
+    //Generic Constructor for Handling Different Type Object
+    JavaGenericDemo(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
     //compareTo method for Checking Maximum from 3 Integer Object
-    public <T extends Comparable<T>> T compareTo(T a,T b,T c) {
+    public T compareTo() {
         if(a.compareTo(b) > 0){
             if(a.compareTo(c) > 0) {
                 return a;
@@ -20,15 +28,15 @@ public class JavaGenericDemo {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Java Core - Generics Test Maximum.");
+        System.out.println("Welcome to Java Core - Generics Test Maximum Developed.");
         //Creating Generic Integer Object for Integer Comparison
-        JavaGenericDemo integerComparison = new JavaGenericDemo();
-        System.out.println("Maximum from 3 Integer Object : "+integerComparison.compareTo(90,120,190));
+        JavaGenericDemo<Integer> integerComparison = new JavaGenericDemo(90,120,190);
+        System.out.println("Maximum from 3 Integer Object : "+integerComparison.compareTo());
         //Creating Generic Float Object for Float Comparison
-        JavaGenericDemo floatComparison = new JavaGenericDemo();
-        System.out.println("Maximum from 3 Float Object : "+floatComparison.compareTo(90.0F,120.0F,190.0F));
+        JavaGenericDemo<Float> floatComparison = new JavaGenericDemo(90.0F,120.0F,190.0F);
+        System.out.println("Maximum from 3 Float Object : "+floatComparison.compareTo());
         //Creating Generic String Object for String Comparison
-        JavaGenericDemo stringComparison = new JavaGenericDemo();
-        System.out.println("Maximum from 3 String Object : "+stringComparison.compareTo("Apple","Peach","Banana"));
+        JavaGenericDemo<String> stringComparison = new JavaGenericDemo("Apple","Peach","Banana");
+        System.out.println("Maximum from 3 String Object : "+stringComparison.compareTo());
     }
 }
