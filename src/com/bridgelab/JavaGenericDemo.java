abec1a40d488b09042791695cadfc0af9b554a5a
nitis <nitish8152@gmail.com>
2022-10-11 13:14:05 +0530

    Added: Generic Based Problem

---
+++ src/com/bridgelab/JavaGenericDemo.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class JavaGenericDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Core - Generics Test Maximum.");
+    }
+}
